852f5f208f430a2e2b562e5ded378c12f054c08f
Bruno Coelho <bruno-l-coelho@telecom.pt>
2013-07-10 14:04:28 +0100

    cleaned up a bit

--- ruby/selfpipe.rb
+++ ruby/selfpipe.rb
@@ -1,17 +1,13 @@
 class SelfPipeSample
   HANDLED_SIGNALS = [ :TERM, :USR1, :USR2 ]
-  OTHER_PROCESSES = [ :HUP ] #T
 
   def initialize
     @selfpipe_reader, @selfpipe_writer = create_pipe
     @signal_queue  = []
-    @process_reader, @process_writer = create_pipe #T
-    puts Process.pid
   end
 
   def start
     register_signals
-    register_other_processes
     signals_listener
   end
 
@@ -19,19 +15,16 @@ class SelfPipeSample
   # Handle an INT signal
   def handle_term
     puts "SIGTERM received"
-#    raise "not implemented #{__method__}"
   end
 
   # Handle a USR1 signal
   def handle_usr1
     puts "SIGUSR1 received"
-#    raise "not implemented #{__method__}"
   end
 
   # Handle a USR2 signal
   def handle_usr2
     puts "SIGUSR2 received"
-#    raise "not implemented #{__method__}"
   end
 
   ############################# PRIVATE #################################
@@ -45,19 +38,8 @@ class SelfPipeSample
   def register_signals
     HANDLED_SIGNALS.each do |signal|
 	trap(signal) do 
-           puts "TRAPING SIGNAL"
            @signal_queue << signal
            notice_signal 
-        end if Signal.list.include? signal.to_s
-    end
-  end
-
-  #T
-  def register_other_processes
-    OTHER_PROCESSES.each do |signal|
-	trap(signal) do 
-           puts "TRAPING PROCESS"
-           @process_writer.write_nonblock('.')
         end if Signal.list.include? signal.to_s
     end
   end
@@ -74,11 +56,7 @@ class SelfPipeSample
   def signals_listener
     begin
       loop do
-
-sleep 5
-
-        io = IO.select([@selfpipe_reader] + [@process_reader])
-        #io = IO.select([@process_reader])
+        io = IO.select([@selfpipe_reader])
 
         if io[0].include?(@selfpipe_reader)
           @selfpipe_reader.read_nonblock(1)
@@ -86,15 +64,6 @@ sleep 5
           while sig = @signal_queue.pop
             signal_handling sig
           end
-          puts "*sleep signal"
-	  sleep 5
-        end
-
-        if io[0].include?(@process_reader)
-          @process_reader.read_nonblock(1)
-          puts "HUP received"
-          puts "*sleep process"
-          sleep 5
         end
       end
     rescue Exception => ex
